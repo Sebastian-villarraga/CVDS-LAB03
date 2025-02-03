2c2e886f013d9e277d202b3881d69afb2d2c5c94
sebastian.vguerrero <sebastian.vguerrero@mail.escuelaing.edu.co>
2025-02-03 12:23:38 -0500

    actualizacion directorios

--- Library/src/main/test/edu/eci/cvds/tdd/library/LibraryTest.java
+++ Library/src/main/test/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -1,9 +1,13 @@
+package edu.eci.cvds.tdd.library;
+
 import org.junit.jupiter.api.Test;
 import edu.eci.cvds.tdd.library.Library;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LibraryTest {
     @Test
     public void testExample(){
-
+       assertTrue(true);
     }
 }
