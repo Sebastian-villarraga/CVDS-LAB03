4998d8b4a37be93b009850693565b9213a8ee02d
sebastian.vguerrero <sebastian.vguerrero@mail.escuelaing.edu.co>
2025-02-06 12:46:14 -0500

    adicion de pruebas en UserTest

--- Library/src/main/test/edu/eci/cvds/tdd/library/user/UserTest.java
+++ Library/src/main/test/edu/eci/cvds/tdd/library/user/UserTest.java
@@ -18,4 +18,46 @@ public class UserTest {
         user.setId("Sebastian21");
         assertEquals("Sebastian21", user.getId());
     }
+
+    @Test
+    public void testSetNameNull() {
+        User user = new User();
+        user.setName(null);
+        assertNull(user.getName(), "El nombre no debería ser nulo");
+    }
+
+    @Test
+    public void testSetIdNull() {
+        User user = new User();
+        user.setId(null);
+        assertNull(user.getId(), "El ID no debería ser nulo");
+    }
+
+    @Test
+    public void testSetNameEmpty() {
+        User user = new User();
+        user.setName("");
+        assertEquals("", user.getName(), "El nombre no debería estar vacío");
+    }
+
+    @Test
+    public void testSetIdEmpty() {
+        User user = new User();
+        user.setId("");
+        assertEquals("", user.getId(), "El ID no debería estar vacío");
+    }
+
+    @Test
+    public void testSetNameWithSpecialCharacters() {
+        User user = new User();
+        user.setName("!@# $%^&*()");
+        assertEquals("!@# $%^&*()", user.getName(), "El nombre con caracteres especiales debería ser aceptado");
+    }
+
+    @Test
+    public void testSetIdWithSpecialCharacters() {
+        User user = new User();
+        user.setId("user!@123");
+        assertEquals("user!@123", user.getId(), "El ID con caracteres especiales debería ser aceptado");
+    }
 }
